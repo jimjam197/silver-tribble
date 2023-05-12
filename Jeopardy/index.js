e566c8b5a0997280c336f46af1537afe690b44e6
jimjam197 <Sjc97@live.com>
2023-05-11 21:29:48 -0400

    upadates

--- Jeopardy/index.js
+++ Jeopardy/index.js
@@ -15,7 +15,7 @@ for (let i = 1; i < 6; i++) {
 div.addEventListener('click', () => {
     // is telling the webpage to wait and listen for someone to click the div element
     const qObj = qs[parseInt(j) - 1][parseInt(round) - 1][i.toString() + '00']//is telling the computer to go and find some info and turning the string into a number and adding 00 to the end
-    console.log(qObj)//overall its sayying find me this info and show it when i click on the element
+    console.log(qObj)//overall its sayying find me this info and show it when clicked on the element
 })
 }
 }
