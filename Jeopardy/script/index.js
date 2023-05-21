b32d14935ae612412e0fa2cbcb3ab4d66ddddef5
jimjam197 <Sjc97@live.com>
2023-05-21 17:56:30 -0400

    got farther but still trying to figure it out

--- Jeopardy/script/index.js
+++ Jeopardy/script/index.js
@@ -1,40 +1,81 @@
-import qs from './rounds.js';
+import qs from 'rounds.js'
+const rounds = require('./rounds.js');
 
+const round = document.getElementById('round-num').innerText;
+
+let player1Points = 0;
+let player2Points = 0;
+let currentPlayer = 'Player 1';
+
+// Function to close the modal
 function closeModal(modalId) {
   const modal = document.getElementById(modalId);
   modal.style.display = 'none';
 }
 
-for (let i = 1; i < 6; i++) {
-  for (const div of document.querySelectorAll(`.r${i}`)) {
-    const divClasses = div.className.split(' ');
-    const colClass = divClasses.find(c => c.startsWith('c'));
-    const j = colClass[1];
-    const round = document.getElementById('round-num').innerText;
+// Attach click event listener to the grid container
+document.getElementById('grid-container').addEventListener('click', (event) => {
+  const target = event.target;
+  const isCell = target.classList.contains('grid-cell');
+  if (!isCell) return;
 
-    div.addEventListener('click', () => {
-      const qObj = qs[parseInt(j) - 1][parseInt(round) - 1][i.toString() + '00'];
-      console.log(qObj);
+  const row = target.dataset.row;
+  const col = target.dataset.col;
 
-      const modal = document.getElementById(`modal-r${i}c${j}`);
-      modal.style.display = 'block';
+  // Get the question object based on the row, column, and round
+  const qObj = rounds[col - 1].categories[round - 1].questions[row - 1];
+  console.log(qObj);
 
-      let span = modal.querySelector('.close');
-      let btn = modal.querySelector('button');
+  const modal = document.getElementById(`modal-r${row}c${col}`);
+  modal.style.display = 'block';
 
-      btn.onclick = function () {
-        modal.style.display = 'none';
-      };
+  const span = modal.querySelector('.close');
+  const btn = modal.querySelector('button');
+  const input = modal.querySelector('input');
+  const modalText = modal.querySelector('p');
 
-      span.onclick = function () {
-        modal.style.display = 'none';
-      };
+  // Set the modal text to the corresponding question
+  modalText.textContent = qObj.question;
 
-      window.onclick = function (event) {
-        if (event.target === modal) {
-          modal.style.display = 'none';
-        }
-      };
-    });
-  }
-}
+  // Button click handler to check the submitted answer and update points
+  btn.addEventListener('click', () => {
+    const submittedAnswer = input.value.trim().toLowerCase();
+    const correctAnswer = qObj.answer.toLowerCase();
+
+    if (submittedAnswer === correctAnswer) {
+      // Answer is correct
+      alert('Correct!');
+      // Perform actions for a correct answer
+      if (currentPlayer === 'Player 1') {
+        player1Points++;
+      } else {
+        player2Points++;
+      }
+    } else {
+      // Answer is incorrect
+      alert('Incorrect!');
+    }
+
+    // Update turn
+    currentPlayer = currentPlayer === 'Player 1' ? 'Player 2' : 'Player 1';
+
+    // Update score display
+    document.getElementById('player1-points').textContent = player1Points;
+    document.getElementById('player2-points').textContent = player2Points;
+    document.getElementById('current-turn').textContent = currentPlayer;
+
+    closeModal(`modal-r${row}c${col}`);
+  });
+
+  // Close button click handler to close the modal
+  span.addEventListener('click', () => {
+    closeModal(`modal-r${row}c${col}`);
+  });
+
+  // Close the modal when clicking outside the modal
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      closeModal(`modal-r${row}c${col}`);
+    }
+  });
+});
