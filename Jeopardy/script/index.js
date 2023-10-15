0944e56a0a950bb8f3065c3c6e7f065496246c57
jimjam197 <Sjc97@live.com>
2023-10-14 21:50:51 -0400

    added js workings for unit 4 capabilities

--- Jeopardy/script/index.js
+++ Jeopardy/script/index.js
@@ -1,81 +1,209 @@
-const qs = require('./rounds.js');
+const qs = require("./rounds.js");
 
-let player1Points = 0
-let player2Points = 0
-let currentPlayer = 'Player 1'
+let player1Name = "";
+let player2Name = "";
+let currentPlayer = player1Name;
+document.getElementById("start-game-button").addEventListener("click", () => {
+  player1Name = document.getElementById("player1-name").value;
+  player2Name = document.getElementById("player2-name").value;
 
-// Function to close the modal
-function closeModal(modalId) {
-  const modal = document.getElementById(modalId)
-  modal.style.display = 'none'
-}
+  document.getElementById("player1-display").textContent = player1Name;
+  document.getElementById("player2-display").textContent = player2Name;
 
-// Loop through rows and columns
-for (let i = 1; i < 6; i++) {
-  for (const div of document.querySelectorAll(`.r${i}`)) {
-    const divClasses = div.className.split(' ')
-    const colClass = divClasses.find(c => c.startsWith('c'))
-    const j = colClass[1]
-    const round = document.getElementById('round-num').innerText
+  currentPlayer = player1Name;
+  document.getElementById("current-player-display").textContent = currentPlayer;
+  document.getElementById(
+    "current-turn-display"
+  ).textContent = `It's ${currentPlayer}'s turn`;
+});
 
-    // Add click event listener to each div
-    div.addEventListener('click', () => {
-      const qObj = qs[parseInt(j) - 1][parseInt(round) - 1][i.toString() + '00']
-      console.log(qObj)
+let player1Points = 0;
+let player2Points = 0;
+let currentRound = 1;
+let selectedQuestions = [];
 
-      const modal = document.getElementById(`modal-r${i}c${j}`)
-      modal.style.display = 'block'
-
-      let span = modal.querySelector('.close')
-      let btn = modal.querySelector('button')
-      let input = modal.querySelector('input')
-      let modalText = modal.querySelector('p')
-
-      // Set the modal text to the corresponding question
-      modalText.textContent = qObj.question
-
-      // Button click handler to check the submitted answer and update points
-      btn.onclick = function () {
-        const submittedAnswer = input.value.trim().toLowerCase()
-        const correctAnswer = qObj.answer.toLowerCase()
-
-        if (submittedAnswer === correctAnswer) {
-          // Answer is correct
-          alert('Correct!')
-          // Perform actions for a correct answer
-          if (currentPlayer === 'Player 1') {
-            player1Points++
-          } else {
-            player2Points++
-          }
-        } else {
-          // Answer is incorrect
-          alert('Incorrect!')
-          //Inform result
-        }
-
-        // Update turn
-        currentPlayer = currentPlayer === 'Player 1' ? 'Player 2' : 'Player 1'
-
-        // Update score display
-        document.getElementById('player1-points').textContent = player1Points
-        document.getElementById('player2-points').textContent = player2Points
-        document.getElementById('current-turn').textContent = currentPlayer
-
-        modal.style.display = 'none'
-      }
-
-      // Close button click handler to close the modal
-      span.onclick = function () {
-        modal.style.display = 'none'
-      }
-
-      // Close the modal when clicking outside the modal
-      window.onclick = function (event) {
-        if (event.target === modal) {
-          modal.style.display = 'none'
-        }
-      }
-    })
+for (let i = 0; i < 5; i++) {
+  selectedQuestions[i] = [];
+  for (let j = 0; j < 6; j++) {
+    selectedQuestions[i][j] = false;
   }
 }
+
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.style.display = "none";
+}
+function displayQuestion(i, j) {
+  if (selectedQuestions[i - 1][j - 1]) {
+    return;
+  }
+  const qObj = qs[j - 1][currentRound - 1][i.toString() + "00"];
+  const modal = document.getElementById(`modal-r${i}c${j}`);
+  modal.style.display = "block";
+
+  const span = modal.querySelector(".close");
+  const btn = modal.querySelector("button");
+  const input = modal.querySelector("input");
+  const modalText = modal.querySelector("p");
+
+  modalText.textContent = qObj.question;
+
+  btn.onclick = function () {
+    const submittedAnswer = input.value.trim().toLowerCase();
+    const correctAnswer = qObj.answer.toLowerCase();
+
+    if (submittedAnswer === correctAnswer) {
+      alert("Correct!");
+
+      if (qObj.isDailyDouble) {
+        if (currentPlayer === player1Name) {
+          player1Points += qObj.points * 2;
+        } else {
+          player2Points += qObj.points * 2;
+        }
+      } else {
+        if (currentPlayer === player1Name) {
+          player1Points += qObj.points;
+        } else {
+          player2Points += qObj.points;
+        }
+      }
+
+      document.getElementById("player1-points").textContent = player1Points;
+      document.getElementById("player2-points").textContent = player2Points;
+
+      currentPlayer = currentPlayer === player1Name ? player2Name : player1Name;
+
+      document.getElementById(
+        "current-turn-display"
+      ).textContent = `It's ${currentPlayer}'s turn`;
+
+      modal.style.display = "none";
+      endRound();
+    } else {
+      alert("Incorrect!");
+
+      currentPlayer = currentPlayer === player1Name ? player2Name : player1Name;
+
+      document.getElementById(
+        "current-turn-display"
+      ).textContent = `It's ${currentPlayer}'s turn`;
+    }
+  };
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+}
+
+function passQuestion(i, j) {
+  if (currentPlayer === "Player 1") {
+    currentPlayer = "Player 2";
+  } else {
+    currentPlayer = "Player 1";
+  }
+
+  document.getElementById("current-turn").textContent = currentPlayer;
+  player1Points -= 100;
+  player2Points -= 100;
+
+  document.getElementById("player1-points").textContent = player1Points;
+  document.getElementById("player2-points").textContent = player2Points;
+
+  closeModal(`modal-r${i}c${j}`);
+}
+
+function endRound() {
+  if (isBoardCleared() && currentRound < 2) {
+    alert(`Move on to Round ${currentRound + 1}`);
+    currentRound++;
+    document.getElementById("round-num").textContent = currentRound;
+    enableNextRoundButton();
+  } else if (currentRound === 2 && isBoardCleared()) {
+    alert("End of the game. Determine the winner.");
+    determineWinner();
+  }
+}
+
+function isBoardCleared() {
+  for (let i = 0; i < selectedQuestions.length; i++) {
+    for (let j = 0; j < selectedQuestions[i].length; j++) {
+      if (!selectedQuestions[i][j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+function determineWinner() {
+  let winner;
+  if (player1Points > player2Points) {
+    winner = "Player 1";
+  } else if (player2Points > player1Points) {
+    winner = "Player 2";
+  } else {
+    winner = "It's a tie!";
+  }
+
+  alert(`${winner} wins!`);
+}
+
+function enableNextRoundButton() {
+  if (currentRound === 2) {
+    document.getElementById("round2Button").disabled = false;
+  }
+}
+for (let i = 1; i < 6; i++) {
+  for (let j = 1; j < 7; j++) {
+    const gridItem = document.querySelector(`.q.r${i}.c${j}`);
+    gridItem.addEventListener("click", () => {
+      if (!selectedQuestions[i - 1][j - 1]) {
+        selectedQuestions[i - 1][j - 1] = true;
+        displayQuestion(i, j);
+      }
+    });
+  }
+}
+
+for (let i = 1; i < 6; i++) {
+  for (let j = 1; j < 7; j++) {
+    const passButton = document.querySelector(`.pass.r${i}.c${j}`);
+    passButton.addEventListener("click", () => {
+      passQuestion(i, j);
+    });
+  }
+}
+
+function resetBoard() {
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 7; j++) {
+      selectedQuestions[i][j] = false;
+      const gridItem = document.querySelector(`.q.r${i + 1}.c${j + 1}`);
+      gridItem.classList.remove("selected");
+    }
+  }
+
+  currentRound = 1;
+  document.getElementById("round-num").textContent = currentRound;
+
+  currentPlayer = "Player 1";
+  document.getElementById("current-turn").textContent = currentPlayer;
+}
+
+document.getElementById("round2Button").addEventListener("click", () => {
+  if (currentRound === 1 && isBoardCleared()) {
+    currentRound = 2;
+    document.getElementById("round-num").textContent = currentRound;
+    document.getElementById("round2Button").disabled = true;
+    resetBoard();
+  }
+});
+
+document.getElementById("finalRoundButton").addEventListener("click", () => {
+  if (currentRound === 2 && isBoardCleared()) {
+    currentRound = 3;
+    document.getElementById("round-num").textContent = currentRound;
+    document.getElementById("finalRoundButton").disabled = true;
+    resetBoard();
+  }
+});
--- Jeopardy/script/rounds.js
+++ Jeopardy/script/rounds.js
@@ -8,7 +8,8 @@ export default [
       },
       200: {
         category: "Classes and Races",
-        question: "What class is known for their strong sense of honor and duty?",
+        question:
+          "What class is known for their strong sense of honor and duty?",
         answer: "Paladine",
       },
       300: {
@@ -25,8 +26,9 @@ export default [
         category: "Classes and Races",
         question: "What race has a natural affinity for tinkering",
         answer: "Gnomes",
-      }
-    }, {
+      },
+    },
+    {
       100: {
         category: "Classes and Races",
         question:
@@ -41,31 +43,36 @@ export default [
       },
       300: {
         category: "Classes and Races",
-        question: "What is the name of the D&D class that specializes in manipulating time and space?",
+        question:
+          "What is the name of the D&D class that specializes in manipulating time and space?",
         answer: "Chronomancer",
       },
       400: {
         category: "Classes and Races",
-        question: "What is the name of the D&D race that is known for its affinity with nature and shape-shifting abilities?",
+        question:
+          "What is the name of the D&D race that is known for its affinity with nature and shape-shifting abilities?",
         answer: "Shifters",
       },
       500: {
         category: "Classes and Races",
-        question: "What is the name of the D&D class that focuses on controlling undead creatures and manipulating necrotic energy?",
+        question:
+          "What is the name of the D&D class that focuses on controlling undead creatures and manipulating necrotic energy?",
         answer: "Necromancer",
-      }
-    }
+      },
+    },
   ],
   [
     {
       100: {
         category: "Spells",
-        question: "What spell creates a magical shield around the caster, protecting them from harm?",
+        question:
+          "What spell creates a magical shield around the caster, protecting them from harm?",
         answer: "Mage Armor",
       },
       200: {
         category: "Spells",
-        question: "What spell allows the caster to teleport themselves to another location?",
+        question:
+          "What spell allows the caster to teleport themselves to another location?",
         answer: "Teleport",
       },
       300: {
@@ -76,47 +83,56 @@ export default [
       },
       400: {
         category: "Spells",
-        question: "What spell causes a creature to fall asleep and become incapacitated?",
+        question:
+          "What spell causes a creature to fall asleep and become incapacitated?",
         answer: "Sleep",
       },
       500: {
         category: "Spells",
-        question: "What spell summons a powerful elemental creature to do the caster's bidding?",
+        question:
+          "What spell summons a powerful elemental creature to do the caster's bidding?",
         answer: "Conjure Elemental",
-      }
-    }, {
+      },
+    },
+    {
       100: {
         category: "Spells",
-        question: " What is the name of the spell that creates a spectral hand that can manipulate objects and disarm traps from a distance?",
+        question:
+          " What is the name of the spell that creates a spectral hand that can manipulate objects and disarm traps from a distance?",
         answer: "Mage Hand",
       },
       200: {
         category: "Spells",
-        question: "What spell allows a spellcaster to create a zone of silence that muffles all sound within its area of effect?",
+        question:
+          "What spell allows a spellcaster to create a zone of silence that muffles all sound within its area of effect?",
         answer: "Silence",
       },
       300: {
         category: "Spells",
-        question: "What spell allows a spellcaster to take control of a creature and use it to do their bidding?",
+        question:
+          "What spell allows a spellcaster to take control of a creature and use it to do their bidding?",
         answer: "Dominate Person",
       },
       400: {
         category: "Spells",
-        question: "What is the name of the spell that allows a spellcaster to create an illusory double of themselves to confuse their enemies?",
+        question:
+          "What is the name of the spell that allows a spellcaster to create an illusory double of themselves to confuse their enemies?",
         answer: "Mirror Image",
       },
       500: {
         category: "Spells",
-        question: "What spell allows a spellcaster to summon a powerful, undead creature to fight for them?",
+        question:
+          "What spell allows a spellcaster to summon a powerful, undead creature to fight for them?",
         answer: "Create Undead",
-      }
-    }
+      },
+    },
   ],
   [
     {
       100: {
         category: "Monsters and Creatures",
-        question: "What monster is known for its ability to petrify its victims with a gaze?",
+        question:
+          "What monster is known for its ability to petrify its victims with a gaze?",
         answer: "Medusa",
       },
       200: {
@@ -126,20 +142,24 @@ export default [
       },
       300: {
         category: "Monsters and Creatures",
-        question: "What monster is a giant, spider-like creature that can paralyze its prey?",
+        question:
+          "What monster is a giant, spider-like creature that can paralyze its prey?",
         answer: "Phase Spider",
       },
       400: {
         category: "Monsters and Creatures",
-        question: "What monster is a shape-shifter that can take on the form of any humanoid creature?",
+        question:
+          "What monster is a shape-shifter that can take on the form of any humanoid creature?",
         answer: "Doppelganger",
       },
       500: {
         category: "Monsters and Creatures",
-        question: "What monster is a powerful aberration creature that can control the minds of others?",
+        question:
+          "What monster is a powerful aberration creature that can control the minds of others?",
         answer: "Mind Flayer",
       },
-    }, {
+    },
+    {
       100: {
         category: "Monsters and Creatures",
         question:
@@ -148,12 +168,14 @@ export default [
       },
       200: {
         category: "Monsters and Creatures",
-        question: "What is the name of the monster that has a powerful breath weapon that can freeze its enemies?",
+        question:
+          "What is the name of the monster that has a powerful breath weapon that can freeze its enemies?",
         answer: "White Dragon",
       },
       300: {
         category: "Monsters and Creatures",
-        question: "What is the name of the undead creature that is created when a humanoid dies with a strong desire for revenge or unfinished business?",
+        question:
+          "What is the name of the undead creature that is created when a humanoid dies with a strong desire for revenge or unfinished business?",
         answer: "Revenant",
       },
       400: {
@@ -164,17 +186,17 @@ export default [
       },
       500: {
         category: "Monsters and Creatures",
-        question: "What is the name of the elemental prince who is known for his association with water and storms?",
+        question:
+          "What is the name of the elemental prince who is known for his association with water and storms?",
         answer: "Olhydra",
-      }
-    }
+      },
+    },
   ],
   [
     {
       100: {
         category: "Lore and History",
-        question:
-          "Who is the god of death in the D&D pantheon?",
+        question: "Who is the god of death in the D&D pantheon?",
         answer: "Vecna",
       },
       200: {
@@ -201,10 +223,12 @@ export default [
           " What is the name of the legendary sword that is said to have been wielded by the hero who defeated the great dragon Tiamat?",
         answer: "Bahamut's Fang",
       },
-    }, {
+    },
+    {
       100: {
         category: "Lore and History",
-        question: "What is the name of the legendary dwarven city that is said to be located deep beneath the mountains?",
+        question:
+          "What is the name of the legendary dwarven city that is said to be located deep beneath the mountains?",
         answer: "Moradin's Forge",
       },
       200: {
@@ -215,7 +239,8 @@ export default [
       },
       300: {
         category: "Lore and History",
-        question: "What is the name of the demon lord who is known for his association with deception and lies?",
+        question:
+          "What is the name of the demon lord who is known for his association with deception and lies?",
         answer: "Fraz-Urb'luu",
       },
       400: {
@@ -229,14 +254,15 @@ export default [
         question:
           "What is the name of the powerful artifact that is said to grant immense power to its wielder, but also drives them insane over time?",
         answer: "The Rod of Seven Parts",
-      }
-    }
+      },
+    },
   ],
   [
     {
       100: {
         category: "Magic Items",
-        question: "What is the name of the magical cloak that grants its wearer the ability to fly?",
+        question:
+          "What is the name of the magical cloak that grants its wearer the ability to fly?",
         answer: "Cloak of the Bat",
       },
       200: {
@@ -247,12 +273,14 @@ export default [
       },
       300: {
         category: "Magic Items",
-        question: "What is the name of the magical sword that deals extra damage to fiends and undead?",
+        question:
+          "What is the name of the magical sword that deals extra damage to fiends and undead?",
         answer: "Holy Avenge",
       },
       400: {
         category: "Magic Items",
-        question: "What is the name of the magical staff that allows its wielder to cast fireball and other powerful spells?",
+        question:
+          "What is the name of the magical staff that allows its wielder to cast fireball and other powerful spells?",
         answer: "Staff of Power",
       },
       500: {
@@ -260,26 +288,31 @@ export default [
         question:
           " What is the name of the magical wand that can turn enemies into harmless creatures?",
         answer: "Wand of Wonder",
-      }
-    }, {
+      },
+    },
+    {
       100: {
         category: "Magic Items",
-        question: "What is the name of the magical artifact that grants its wielder immense power, but also corrupts them over time?",
+        question:
+          "What is the name of the magical artifact that grants its wielder immense power, but also corrupts them over time?",
         answer: "The Deck of Many Things",
       },
       200: {
         category: "Magic Items",
-        question: "What is the name of the magical shield that can deflect spells and other magical attacks?",
+        question:
+          "What is the name of the magical shield that can deflect spells and other magical attacks?",
         answer: "Shield of the Hidden Lord",
       },
       300: {
         category: "Magic Items",
-        question: "What is the name of the magical potion that grants its drinker the ability to breathe underwater?",
+        question:
+          "What is the name of the magical potion that grants its drinker the ability to breathe underwater?",
         answer: "Potion of Water Breathing",
       },
       400: {
         category: "Magic Items",
-        question: "What is the name of the magical boots that allow their wearer to move silently and without leaving a trace?",
+        question:
+          "What is the name of the magical boots that allow their wearer to move silently and without leaving a trace?",
         answer: "Boots of Elvenkind",
       },
       500: {
@@ -287,41 +320,46 @@ export default [
         question:
           "What is the name of the magical necklace that allows its wearer to speak and understand any language?",
         answer: "Amulet of the Planes",
-      }
-    }
+      },
+    },
   ],
   [
     {
       100: {
         category: "D&D Settings",
-        question:
-          " In what D&D setting would you find the city of Waterdeep?",
+        question: " In what D&D setting would you find the city of Waterdeep?",
         answer: "Forgotten Realms",
       },
       200: {
         category: "D&D Settings",
-        question: " What is the name of the desert continent in the Dark Sun setting?",
+        question:
+          " What is the name of the desert continent in the Dark Sun setting?",
         answer: "Athas",
       },
       300: {
         category: "D&D Settings",
-        question: "What is the name of the plane of existence in D&D that is home to demons and other chaotic evil creatures?",
+        question:
+          "What is the name of the plane of existence in D&D that is home to demons and other chaotic evil creatures?",
         answer: "Abyss",
       },
       400: {
         category: "D&D Settings",
-        question: " In what D&D setting would you find the cities of Neverwinter and Baldur's Gate?",
+        question:
+          " In what D&D setting would you find the cities of Neverwinter and Baldur's Gate?",
         answer: "Sword Coast",
       },
       500: {
         category: "D&D Settings",
-        question: "What is the name of the plane of existence in D&D that is home to angels and other lawful good creatures?",
+        question:
+          "What is the name of the plane of existence in D&D that is home to angels and other lawful good creatures?",
         answer: "Celestia",
-      }
-    }, {
+      },
+    },
+    {
       100: {
         category: "D&D Settings",
-        question: "In what D&D setting would you find the city of Sigil, the City of Doors?",
+        question:
+          "In what D&D setting would you find the city of Sigil, the City of Doors?",
         answer: "Planescape",
       },
       200: {
@@ -332,28 +370,30 @@ export default [
       },
       300: {
         category: "D&D Settings",
-        question: " In what D&D setting would you find the city of Sharn, known for its towering skyscrapers?",
+        question:
+          " In what D&D setting would you find the city of Sharn, known for its towering skyscrapers?",
         answer: "Eberron",
       },
       400: {
         category: "D&D Settings",
-        question: " What is the name of the setting that takes place in a world where magic is outlawed and controlled by a powerful organization?",
+        question:
+          " What is the name of the setting that takes place in a world where magic is outlawed and controlled by a powerful organization?",
         answer: "Spelljammer",
       },
       500: {
         category: "D&D Settings",
-        question: "What is the name of the plane of existence in D&D that is home to demons, devils, and other fiendish creatures?",
+        question:
+          "What is the name of the plane of existence in D&D that is home to demons, devils, and other fiendish creatures?",
         answer: "Nine Hells",
-      }
-    }
+      },
+    },
   ],
 
   {
     category: "Final",
     question: "What was the ideal party baised on in 2E?",
     answer: "The Golden Girls",
-  }
+  },
+];
 
-]
-
-module.exports = rounds
+module.exports = rounds;
